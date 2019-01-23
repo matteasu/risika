18626919bb6f6dde8b0dd59b08def640ae6616b5
Matteo Asuni <asunimatteo@gmail.com>
2019-01-23 19:44:39 +0100

    Repo int

---
+++ cmake-build-debug/CMakeFiles/risika.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/risika.dir/main.c.obj"
+  "risika.pdb"
+  "risika.exe"
+  "risika.exe.manifest"
+  "librisika.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/risika.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
